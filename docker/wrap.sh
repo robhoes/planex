7cb12661dec8961112705d0f544942175cf8adcb
Euan Harris <euan.harris@citrix.com>
2016-05-04 16:45:16 +0100

    WIP: Docker: Add a persistent container for /var/cache/mock,yum
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- docker/wrap.sh
+++ docker/wrap.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+docker create \
+  --name planex-persist \
+  -v /var/cache/mock \
+  -v /var/cache/yum \
+  planex-release:0.7.3 /bin/true
+
 docker run \
   --privileged \
   --rm -i -t \
+  --volumes-from planex-persist \
   -v ${PWD}:/build \
   -e EXTUID=$(id -u) -e EXTGID=$(id -g) \
-  planex-release "$*"
+  planex-release:0.7.3 "$*"
