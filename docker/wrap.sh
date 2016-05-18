ee09371e5e18d9cb22056aabf2b1eb8c5423700d
Euan Harris <euan.harris@citrix.com>
2016-05-18 10:09:09 +0100

    Remove comment from middle of docker call
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- docker/wrap.sh
+++ docker/wrap.sh
@@ -6,13 +6,13 @@ docker create \
   -v /var/cache/yum \
   planex-release:0.7.3 /bin/true
 
+# Fill in volumes automatically from the pins file
+# Chicken and egg - need to fill it in when pinning as well
 docker run \
   --privileged \
   --rm -i -t \
   --volumes-from planex-persist \
   -v ${PWD}:/build \
-  # Fill these in automatically from the pins file
-  # Chicken and egg - need to fill it in when pinning as well
   -v /local/scratch/euanh/work/planex:/local/scratch/euanh/work/planex \
   -e EXTUID=$(id -u) -e EXTGID=$(id -g) \
   planex-release:0.7.3 "$*"
